eabf9a2024a0c318c5a2be9cbd443965690eba77
iogogarev <iogogarev@edu.hse.ru>
2024-10-22 22:45:58 +0200

    full training baseline (no metrics yet)

--- code/data.py
+++ code/data.py
@@ -1,8 +1,64 @@
 from torchvision import transforms
+import torch
+from torch.utils.data import Dataset
+import os
+from PIL import Image
 
-transform = transforms.Compose([
-    transforms.Resize((64, 96)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))  # Adjust normalization as per dataset
+from torchvision import transforms
+
+# Define the transformation pipeline
+trainval_transform = transforms.Compose([
+    transforms.ToTensor(),          # Convert the image to a PyTorch tensor and scale pixel values to [0.0, 1.0]
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],  # Normalize the tensor with mean and std
+                         std=[0.229, 0.224, 0.225])
 ])
+
+def read_paths_txt(file_path):
+    image_paths = []
+    labels = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            path, label = line.strip().split(' ')
+            image_paths.append(path)
+            labels.append(int(label))
+
+    return image_paths, labels
+
+
+class MorphDataset(Dataset):
+    def __init__(self, dataset_dir, txt_paths, transform=trainval_transform):
+        self.dataset_dir = dataset_dir
+        self.txt_paths = txt_paths
+        self.transform = transform
+
+        self.image_paths = []
+        self.labels = []
+
+        self._collect_data()
+
+    def _collect_data(self):
+        for txt_path in self.txt_paths:
+            new_paths, new_labels = read_paths_txt(os.path.join(self.dataset_dir, txt_path))
+            self.image_paths.extend(new_paths)
+            self.labels.extend(new_labels)
+
+    def __len__(self):
+        """Returns the total number of samples."""
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        """Generates one sample of data."""
+        # Get the image path and label for the given index
+        img_path = self.image_paths[index]
+        label = self.labels[index]
+
+        # Load the image using PIL
+        image = Image.open(os.path.join(self.dataset_dir, img_path)).convert('RGB')  # Convert image to RGB if it's not
+
+        # Apply any transformations if provided
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
 
--- code/run.py
+++ code/run.py
@@ -1,76 +1,88 @@
+import os
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Subset
-import numpy as np
-import json
+import torchvision.models as models
 
+import json
 import argparse
-from models import DebugNN
-from data import transform
 import tqdm
+
+
+from data import MorphDataset
+
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
+def count_parameters(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    return total_params
+
+
 def train(config):
-    # Download the training and test datasets
-    train_dataset = datasets.MNIST(root='datasets', train=True, download=True, transform=transform)
-    test_dataset = datasets.MNIST(root='datasets', train=False, download=True, transform=transform)
+    train_dataset = MorphDataset(dataset_dir=config["dataset_dir"], txt_paths=config["train_txt"])
+    val_dataset = MorphDataset(dataset_dir=config["dataset_dir"], txt_paths=config["val_txt"])
 
-    train_dataset = Subset(train_dataset, list(range(1000)))
+    print(f"Total number of train objects: {len(train_dataset)}.")
+    print(f"Total number of val objects: {len(val_dataset)}.")
 
-    # Create DataLoader for training and test sets
     train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=config["batch_size"], shuffle=False)
 
-    test_loader = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=False)
+    model = models.efficientnet_b0(weights="IMAGENET1K_V1")
+    model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features, 1)
+    print(model)
+    print(f"Total number of parameters in the model: {count_parameters(model)}.")
 
-    model = DebugNN()
-    criterion = nn.BCEWithLogitsLoss()  # Combines a Sigmoid layer and the BCELoss
+    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(config["pos_weight"]))  # Combines a Sigmoid layer and the BCELoss
     optimizer = optim.Adam(model.parameters(), lr=config["learning_rate"])
 
     for epoch in range(config["num_epochs"]):
         running_loss = 0.0
         model.train()
-        for images, labels in tqdm.tqdm(train_loader):
-            images = images.to(DEVICE)
-            labels = (labels == 1)
-            labels = labels.to(DEVICE).float().unsqueeze(1)  # Shape: (batch_size, 1)
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+        for batch_idx, (images, labels) in enumerate(tqdm.tqdm(train_loader)):
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
 
-            # Backward and optimize
+            outputs = model(images)
+            loss = criterion(outputs.squeeze(), labels.float())
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
 
-        print(f'Epoch [{epoch + 1}/{config["num_epochs"]}], Train loss: {running_loss/len(train_dataset):.4f}')
+        print(f'Epoch [{epoch + 1}/{config["num_epochs"]}], Train loss: {running_loss/len(train_dataset):.10f}')
 
         model.eval()
         with torch.no_grad():
             val_running_loss = 0.0
-            for batch_idx, (images, labels) in enumerate(test_loader):
-                images = images.to(DEVICE)
-                labels = (labels == 1)
-                labels = labels.to(DEVICE).float().unsqueeze(1)  # Shape: (batch_size, 1)
-
+            for batch_idx, (images, labels) in enumerate(tqdm.tqdm(val_loader)):
+                images, labels = images.to(DEVICE), labels.to(DEVICE)
 
                 outputs = model(images)
-                loss = criterion(outputs, labels)
+                loss = criterion(outputs.squeeze(), labels.float())
 
                 val_running_loss += loss.item()
 
-        print(f'Epoch [{epoch + 1}/{config["num_epochs"]}], Val loss: {val_running_loss/len(test_dataset):.3f}')
+            torch.save({
+                'epoch': epoch + 1,  # Save the epoch number
+                'model_state_dict': model.state_dict(),  # Save model state
+                'optimizer_state_dict': optimizer.state_dict(),  # Save optimizer state (optional)
+                'val_loss': val_running_loss / len(val_dataset),  # Save current validation loss
+            }, os.path.join(config["save_dir"], f"model_epoch_{epoch+1}.pth"))
+
+        print(f'Epoch [{epoch + 1}/{config["num_epochs"]}], Val loss: {val_running_loss/len(val_dataset):.3f}')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("mode", type=str, help="train / test mode", default="train")
     parser.add_argument("--config", type=str, help="Path to the training config")
+    parser.add_argument("--datadir", type=str, help="Path to the training config")
 
     # Parse arguments
     args = parser.parse_args()
@@ -78,6 +90,10 @@ if __name__ == "__main__":
     with open(args.config, 'r') as file:
         config = json.load(file)
 
+    os.mkdir(f"./logs/{config['name']}")
+
+    config["dataset_dir"] = args.datadir
+    config["save_dir"] = f"./logs/{config['name']}"
     train(config)
 
 
